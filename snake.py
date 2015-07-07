b208fe34dbaa09766c1a761909e52889f3e31a26
Thomas Ballinger <thomasballinger@gmail.com>
2015-07-07 12:33:56 -0400

    add color to border

--- snake.py
+++ snake.py
@@ -1,10 +1,13 @@
 import random
 
+def red(s):
+    return '\x1b[31m'+s+'\x1b[39'
+
 board = [[random.choice([0, 1])]
          for _ in range(10)]
 
 def display(board):
-    print '-'*len(board[0])
+    print red('-'*len(board[0]))
     for row in board:
         print ''.join('x' if x else ' '
                       for x in row)
