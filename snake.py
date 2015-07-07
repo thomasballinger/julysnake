ae2fb71dcc75838f8a0aab58b3b4e64b373f7f2b
esander <esander@uchicago.edu>
2015-07-07 12:33:42 -0400

    documented snake file

--- snake.py
+++ snake.py
@@ -1,13 +1,17 @@
 import random
 
+## create random board
 board = [[random.choice([0, 1])]
          for _ in range(10)]
 
 def display(board):
+    ## top border
     print '-'*len(board[0])
+    ## display board
     for row in board:
         print ''.join('x' if x else ' '
                       for x in row)
+    ## bottom border
     print '-'*len(board[0])
 
 display(board)
