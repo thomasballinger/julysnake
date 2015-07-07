6b46b4fd338d7a0b63a4e3db241dd36c39eec01e
Thomas Ballinger <thomasballinger@gmail.com>
2015-07-07 11:43:35 -0400

    better display function

--- snake.py
+++ snake.py
@@ -1,10 +1,13 @@
 
 
-board = [[0] * 10 for _ in range(10)]
+board = [[1] * 10 for _ in range(10)]
 
 def display(board):
+    print '-'*len(board[0])
     for row in board:
-        print row
+        print ''.join('x' if x else ' '
+                      for x in row)
+    print '-'*len(board[0])
 
 display(board)
 
